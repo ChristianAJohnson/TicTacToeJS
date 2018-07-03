5734e4853ee57a5a708c1f6d52653edd02e347ec
ChristianAJohnson <chrisgochris@gmail.com>
2018-07-03 14:55:04 -0400

    for loop created for click eventlistener for every box

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 function DisplayBoard()
 {
-	for(i=0;i<9;i++)
+	for(let i=0;i<9;i++)
 	{
 		// this was to combine the word "box" and the number from the for loop (i) so that they can be assigned unqiue id's 
-		var temp1 = "boxid"
-		var temp2 = i.toString()
-		var box = temp1.concat(temp2)
+		// var temp1 = "boxid"
+		// var temp2 = i.toString()
+		// var box = temp1.concat(temp2)
 
 		var newbox = document.createElement("div")
 		newbox.style.height = "150px"
 		newbox.style.width = "150px"
 		newbox.style.display = "inline-block"
-		newbox.setAttribute("id",box) //assign each box an ID
+		// newbox.setAttribute("id",box) //assign each box an ID
+		newbox.setAttribute("id",i) //assign each box an ID from the for loop (i)
 		newbox.setAttribute("class","boxclass") // give all the boxes a class called "boxclass"
 
 		if(i==3 || i==6)
@@ -39,22 +40,55 @@ DisplayBoard();
 
 function CheckBoxes()
 {
-	var boxid0 = document.getElementById("boxid0");
-	var boxid1 = document.getElementById("boxid1");
-	var boxid2 = document.getElementById("boxid2");
-	var boxid3 = document.getElementById("boxid3");
-	var boxid4 = document.getElementById("boxid4");
-	var boxid5 = document.getElementById("boxid5");
-	var boxid6 = document.getElementById("boxid6");
-	var boxid7 = document.getElementById("boxid7");
-	var boxid8 = document.getElementById("boxid8");
+	// var boxid0 = document.getElementById("boxid0");
+	// var boxid1 = document.getElementById("boxid1");
+	// var boxid2 = document.getElementById("boxid2");
+	// var boxid3 = document.getElementById("boxid3");
+	// var boxid4 = document.getElementById("boxid4");
+	// var boxid5 = document.getElementById("boxid5");
+	// var boxid6 = document.getElementById("boxid6");
+	// var boxid7 = document.getElementById("boxid7");
+	// var boxid8 = document.getElementById("boxid8");
 
 
-	boxid7.addEventListener("click", function(event)
+	// boxid7.addEventListener("click", function(event)
+	// {
+	// 	boxid7.innerHTML = "&#10006"
+	// 	console.log("You clicked the 7th Box")
+	// })
+var boxclass = document.getElementsByClassName("boxclass")
+	for(let i=0;i<boxclass.length;i++)
 	{
-		console.log("Clicked worked")
-	})
+		boxclass[i].addEventListener("click", function()
+		{
+			boxclass[i].innerHTML = "&#10006"
+			console.log("You clicked box number " + boxclass[i].id)	
+		})
+	}
+
+
 }
+
+function Cross(position)
+{
+
+}
+
+function Zero(position)
+{
+	
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
