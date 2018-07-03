e0d8eb88b3758fb50db29a7923a38d32be7c1789
ChristianAJohnson <chrisgochris@gmail.com>
2018-07-03 11:47:37 -0400

    setting up board backgroud, so it displays in the middle of the screen

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
-function test()
+var board = document.createElement("div")
+board.style.width="800px"
+board.style.height="700px"
+board.style.backgroundColor="yellow"
+document.body.appendChild(board)
+
+function grey()
 {
 
-var greySq = document.createElement('div')
-greySq.style.height = '150px'
-greySq.style.width = '150px'
-greySq.style.backgroundColor = 'grey'
-greySq.style.display = 'inline-block'
-document.body.appendChild(greySq)
+var greySq = document.createElement("div")
+greySq.style.height = "50px"
+greySq.style.width = "50px"
+greySq.style.backgroundColor = "grey"
+greySq.style.display = "inline-block"
+board.body.appendChild(greySq)
 
 }
 
-function test2()
+function red()
 {
 
-var redSq = document.createElement('div')
-redSq.style.height = '150px'
-redSq.style.width = '150px'
-redSq.style.backgroundColor = 'red'
-redSq.style.display = 'inline-block'
-document.body.appendChild(redSq)
-
+var redSq = document.createElement("div")
+redSq.style.height = "50px"
+redSq.style.width = "50px"
+redSq.style.backgroundColor = "red"
+redSq.style.display = "inline-block"
+board.body.appendChild(redSq)
 }
 
-function display()
+function displayBoard()
 {
 	for(i=0;i<9;i++)
 	{
-		if(i%3==0)
+		if(i==3 || i==6)
 		{
-			test()
+			var newline = document.createElement("div")
+			newline.innerHTML = "<br>"
+			board.body.appendChild(newline)
+		}
+		if(i%2==0)
+		{
+			grey()
 		}
 		else
 		{
-			test2()
+			red()
+
 		}
 	}
 	
 }
 
-display();
+// displayBoard();
 
