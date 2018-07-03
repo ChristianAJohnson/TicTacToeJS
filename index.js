bb6d29baf923d886807db8bacec358f8fe1758b8
ChristianAJohnson <chrisgochris@gmail.com>
2018-07-02 22:57:21 -0400

    setting up the Tic-Tac-Toe display

---
+++ index.js
@@ -0,0 +1,27 @@
+function test()
+{
+
+var greySq = document.createElement('div')
+greySq.style.height = '150px'
+greySq.style.width = '150px'
+greySq.style.backgroundColor = 'grey'
+greySq.style.display = 'inline-block'
+document.body.appendChild(greySq)
+
+}
+
+function display()
+{
+	for(i=0;i<7;i++)
+	{
+		test();
+		if(i%3==0)
+		{
+			
+		}
+	}
+	
+}
+
+display();
+
